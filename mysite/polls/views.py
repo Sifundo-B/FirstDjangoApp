b0e116f8ccf6a56f1f2f1a5ad28db2102d1fa68e
Sifundo <sfundobuthelezi2@live.com>
2019-11-15 11:03:40 +0200

    Admin template in Django

---
+++ mysite/polls/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+
+from .models import Question
+# Create your views here.
+def index(request):
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_questions': latest_questions}
+    return render(request, 'polls/index.html', context)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+def result(request, question_id):
+    return  HttpResponse("These are the results of the question: %s" % question_id)
+
+def vote(request, question_id):
+    return  HttpResponse("Vote on question: %s" % question_id)
